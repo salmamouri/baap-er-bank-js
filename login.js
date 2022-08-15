9eabbfe3a3ac57ee10d55a73c3b23bbf7a598b35
salmamouri <mouriclub@gmail.com>
2022-08-16 00:24:08 +0600

    input field added

---
+++ login.js
@@ -0,0 +1,6 @@
+document.getElementById('btn-submit').addEventListener('click',
+function(){
+const emailField = document.getElementById('user-email');
+const email = emailField.value;
+console.log(email);
+}); 

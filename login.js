1174e4fafeb0899b1cab46253f53a4db7c1415b7
salmamouri <mouriclub@gmail.com>
2022-08-16 10:33:30 +0600

    bank & balace added

--- login.js
+++ login.js
@@ -2,5 +2,14 @@ document.getElementById('btn-submit').addEventListener('click',
 function(){
 const emailField = document.getElementById('user-email');
 const email = emailField.value;
-console.log(email);
+
+const passField = document.getElementById('user-password');
+const password = passField.value;
+
+if(email === 'mouri@salma.com'&& password === 'secret'){
+    window.location.href = 'bank.html';
+}
+else {
+   alert('Invalid User');
+}
 }); 
